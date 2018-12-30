f26d6a74bdfef813033a6c5a061bdb96a3630034
LeoFuso <leonardofusonuzzo@gmail.com>
2018-12-30 21:49:26 -0200

    First Commit - Criação de Projeto

---
+++ src/main/java/br/com/lfuso/cursos/spring/restful/order/OrderApplication.java
@@ -0,0 +1,14 @@
+package br.com.lfuso.cursos.spring.restful.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
+
