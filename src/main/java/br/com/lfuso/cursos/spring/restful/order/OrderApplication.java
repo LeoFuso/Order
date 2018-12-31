7a2b344dbbb23093f13d2b9f79e82365b2a6e5b9
LeoFuso <leonardofusonuzzo@gmail.com>
2018-12-30 23:45:39 -0200

    issue-1 Operação de instanciação de objetos default

--- src/main/java/br/com/lfuso/cursos/spring/restful/order/OrderApplication.java
+++ src/main/java/br/com/lfuso/cursos/spring/restful/order/OrderApplication.java
@@ -1,14 +1,31 @@
 package br.com.lfuso.cursos.spring.restful.order;
 
+import br.com.lfuso.cursos.spring.restful.order.domain.Categoria;
+import br.com.lfuso.cursos.spring.restful.order.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class OrderApplication {
+public class OrderApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria informatica = new Categoria(null, "Informática");
+		Categoria escritorio = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(informatica, escritorio));
+	}
 }
 
